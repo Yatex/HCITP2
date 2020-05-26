b7fd62ed7eaea18f30f7e1e1a8d86fc621c04e6d
Yatex <foliver@gmail.com>
2020-05-25 21:14:30 -0300

    cambios de delete button

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -86,41 +86,35 @@
       style="text-align:right;width:50%">
         
        <v-dialog
-        v-model="dialog" max-width="300px" >
+        v-model="dialog" max-width="290px">
               <template v-slot:activator="{ on }">
-                <v-btn
-              
-                rounded
-             v-on="on">
-                  <p style="margin:10px;color:#EC1801">
-                      <v-icon
-                      >mdi-delete</v-icon>
-                       <big> Delete room </big></p>
+                <v-btn rounded v-on="on" color="error">
+                  <v-icon size='30px'>mdi-delete-outline</v-icon>
+                  <p style="margin:10px"> <big>Delete Room</big></p>
                  </v-btn>
               </template>
 
         <v-card>
         <v-card-title>
-          <span class="headline"><i><b>Are you sure?</b></i></span>
+          <span class="headline">Are you sure you want to delete this room?</span>
         </v-card-title>
         <v-card-text>
             If you delete the room all devices inside it will be lost
 
         </v-card-text> 
           <v-divider></v-divider>
-          <v-spacer></v-spacer>
           <v-card-actions>
+          <v-spacer></v-spacer>
           <v-btn
-            color="#F44336"
+          @click="dialog = false">
+            No
+          </v-btn>
+          <v-btn
            @click="deleteRoom();dialog = false;$router.push({ path: '/rooms' });"
            
            >
-           delete
+           Yes
         
-          </v-btn>
-          <v-btn
-          @click="dialog = false">
-            Go back
           </v-btn>
         </v-card-actions>
          </v-card>
